8b27a8753263f57d9ee2205d53ce8633cfa269ef
Patricia Chun <yejeechun@gmail.com>
2017-06-18 19:52:53 -0400

    View function and templates.

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -4,12 +4,15 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import home
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'tweetme.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', home, name='home'),
 ]
 
 
