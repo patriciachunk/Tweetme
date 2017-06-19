65ea6b28ddeb87aeeded828f28a75f19f57b5e52
Patricia Chun <yejeechun@gmail.com>
2017-06-19 16:35:08 -0400

    Associating a user to a Tweet app with foreign keys.

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -5,6 +6,7 @@ from django.db import models
 
 class Tweet(models.Model):
     # user
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
     content     = models.CharField(max_length=140)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
