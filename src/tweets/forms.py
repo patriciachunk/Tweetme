7a39657a576a98604190eb887a41a03c90c6cf6c
Patricia Chun <yejeechun@gmail.com>
2017-06-19 18:31:09 -0400

    Model forms.

---
+++ src/tweets/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Tweet
+
+class TweetModelForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = {
+            "user",
+            "content"
+        }
+        # exclude = ['user']
+
+    def clean_content(self, *args, **kwargs):
+        content = self.cleaned_data.get("content")
+        if content == "abc":
+            raise forms.ValidationError("cannot be ABC")
+        return content
