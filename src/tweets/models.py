aa94baa398cf424fcd92e1994e70f82afff8c5cd
Patricia Chun <yejeechun@gmail.com>
2017-06-19 15:27:42 -0400

    Changing fields in models.

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 
 class Tweet(models.Model):
-    content = models.TextField()
+    # user
+    content     = models.CharField(max_length=140)
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.content)
