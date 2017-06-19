005b7a4a8913205568beebe4fd4da39011a715d1
Patricia Chun <yejeechun@gmail.com>
2017-06-19 18:18:29 -0400

    Dynamic URL routing.

--- src/tweetme/urls.py
+++ src/tweetme/urls.py
@@ -24,8 +24,8 @@ from django.conf.urls.static import static
 from .views import home
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', home, name='home'),
+    url(r'^admin/', admin.site.urls), # admin/
+    url(r'^$', home, name='home'),  # /
     url(r'^tweet/', include('tweets.urls')),
 ]
 
