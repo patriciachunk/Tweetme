5b6d0f08a630bcaece3f2ecb8fdc4c971addc955
Patricia Chun <yejeechun@gmail.com>
2017-06-20 01:41:39 -0400

    Update view.

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,42 +1,28 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
 
 from .forms import TweetModelForm
-from .mixins import FormUserNeededMixin
+from .mixins import FormUserNeededMixin, UserOwnerMixin
 from .models import Tweet
 # Create your views here.
 
 # Create
 
-class TweetCreateView(LoginRequiredMixin, CreateView):
+class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create/"
-    login_url = '/admin/'
-    # redirect_field_name = 'redirect_to'
-
-    # def form_valid(self, form):
-    #     if self.request.user.is_authenticated():
-    #         form.instance.user = self.request.user
-    #         return super(TweetCreateView, self).form_valid(form)
-    #     else:
-    #         form._errors[forms.forms.NON_FIELD_ERRORS] = ErrorList(["User must be logged in to continue. "])
-    #         return self.form_invalid(form)
-
-# def tweet_ceate_view(request):
-#     form = TweetModelForm(request.POST or None)     # making the form happen
-#     if form.is_valid():
-#         instance = form.save(commit=False)
-#         instance.user = request.user
-#         instance.save()
-#     context = {     # form rendered with this context
-#         "form": form
-#     }
-#     return render(request, 'tweets/create_view.html', context)
-
+    # login_url = '/admin/'
 
 # Update
+
+class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    queryset = Tweet.objects.all()
+    form_class = TweetModelForm
+    template_name = 'tweets/update_view.html'
+    success_url = "/tweet/"
+
 
 # Delete
 
