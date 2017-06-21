e48bfdc447f752f1b068dfed4fbf1a0bab2c2c9e
Patricia Chun <yejeechun@gmail.com>
2017-06-21 16:30:20 -0400

    Tweet from homepage.

--- src/tweets/forms.py
+++ src/tweets/forms.py
@@ -3,6 +3,11 @@ from django import forms
 from .models import Tweet
 
 class TweetModelForm(forms.ModelForm):
+    content = forms.CharField(label='',
+                    widget=forms.Textarea(
+                                attrs={'placeholder':"Your message",
+                                    "class":"form-control"}
+                    ))
     class Meta:
         model = Tweet
         fields = {
