25162196b7f2531fc182c8b6200a715e350c9e4d
Patricia Chun <yejeechun@gmail.com>
2017-07-11 16:08:47 -0400

    Ajax & create API view.

--- src/tweets/api/serializers.py
+++ src/tweets/api/serializers.py
@@ -4,7 +4,7 @@ from accounts.api.serializers import UserDisplaySerializer
 from tweets.models import Tweet # ..models import Tweet
 
 class TweetModelSerializer(serializers.ModelSerializer):
-    user = UserDisplaySerializer()
+    user = UserDisplaySerializer(read_only=True)    #write_only
     class Meta:
         model = Tweet
         fields = [
