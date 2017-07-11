c4c1374b29eb52538e11f540b8136564a0066de9
Patricia Chun <yejeechun@gmail.com>
2017-07-11 17:21:04 -0400

    Date display.

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -14,7 +14,7 @@ class TweetCreateAPIView(generics.CreateAPIView):
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
     def get_queryset(self, *args, **kwargs):
-        qs = Tweet.objects.all()
+        qs = Tweet.objects.all().order_by("-timestamp")
         print(self.request.GET)
         query = self.request.GET.get("q", None)
         if query is not None:
--- src/tweets/models.py
+++ src/tweets/models.py
@@ -19,6 +19,10 @@ class Tweet(models.Model):
 
     def get_absolute_url(self):
         return reverse("tweet:detail", kwargs={"pk":self.pk})
+
+    class Meta:
+        ordering = ['-timestamp', 'content']
+
     # def clean(self, *args, **kwargs):
     #     content = self.content
     #     if content == "abc":
