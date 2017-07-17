3069bba4b75ebf622178eaf78cde363b3f107fb5
Patricia Chun <yejeechun@gmail.com>
2017-07-17 17:55:15 -0400

    Pagination & Ajax load more.

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -3,16 +3,21 @@ from rest_framework import generics
 from rest_framework import permissions
 
 from tweets.models import Tweet
+
+from .pagination import StandardResultsPagination
 from .serializers import TweetModelSerializer
 
 class TweetCreateAPIView(generics.CreateAPIView):
     serializer_class = TweetModelSerializer
     permission_classes = [permissions.IsAuthenticated]
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
+    pagination_class = StandardResultsPagination
+    
     def get_queryset(self, *args, **kwargs):
         qs = Tweet.objects.all().order_by("-timestamp")
         print(self.request.GET)
