356699a1c885291dc5d46fcde9e4f7219506eab5
Patricia Chun <yejeechun@gmail.com>
2017-07-18 15:28:53 -0400

    User detail view. Error fixed. KEEP THE TAPS CORRECTTTTTTTT.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -9,8 +9,8 @@ class UserDetailView(DetailView):
     template_name = 'accounts/user_detail.html'
     queryset = User.objects.all()
 
-def get_object(self):
-    return get_object_or_404(
-                User,
-                username__iexact=self.kwargs.get("username")
-                )
+    def get_object(self):
+        return get_object_or_404(
+                    User,
+                    username__iexact=self.kwargs.get("username")
+                    )
