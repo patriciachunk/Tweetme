d4fe74e6b910b58518a9194e1a49c6ff5d46990a
Patricia Chun <yejeechun@gmail.com>
2017-07-18 15:18:52 -0400

    Profile URL & API update.

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 
 from rest_framework import serializers
 
@@ -6,6 +7,8 @@ User = get_user_model()
 
 class UserDisplaySerializer(serializers.ModelSerializer):
     follower_count = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = [
@@ -13,8 +16,12 @@ class UserDisplaySerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'follower_count',
+            'url',
         ]
 
     def get_follower_count(self, obj):
         # print(obj.username)
         return 0
+
+    def get_url(self, obj):
+        return reverse_lazy("profiles:detail", kwargs={"username": obj.username })
