df0432978ead17a30a6bc498988e04f64def4011
Patricia Chun <yejeechun@gmail.com>
2017-07-19 17:52:48 -0400

    Model manager for following.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,12 +3,32 @@ from django.db import models
 
 # Create your models here.
 
+class UserProfileManager(models.Manager):
+    use_for_related_fields = True
+
+    def all(self):
+        qs = self.get_queryset().all()
+        # print(self.instance)    # user
+        try:
+            if self.instance:
+                qs = qs.exclude(user=self.instance)
+        except:
+            pass
+        return qs
+
 class UserProfile(models.Model):
     user        = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')  # user.profile
     following   = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='followed_by')
     # user.profile.following -- users I follow
     # user.followed_by -- users that follow me -- reverse relationship
 
+    objects = UserProfileManager() # UserProfile.objects.all()
+    # abc = UserProfileManager() # UserProfile.abc.all()
+
 
     def __str__(self):
         return str(self.following.all().count())
+
+    def get_following(self):
+        users = self.following.all()    #User.objects.all().exclude(username=self.user.username)
+        return users.exclude(username=self.user.username)
