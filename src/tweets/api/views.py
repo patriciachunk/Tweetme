55c97b350efe5dcd567d8a59955c6722a3c8b95e
Patricia Chun <yejeechun@gmail.com>
2017-07-20 16:22:00 -0400

    Display following tweets.

--- src/tweets/api/views.py
+++ src/tweets/api/views.py
@@ -17,10 +17,10 @@ class TweetCreateAPIView(generics.CreateAPIView):
 class TweetListAPIView(generics.ListAPIView):
     serializer_class = TweetModelSerializer
     pagination_class = StandardResultsPagination
-    
+
     def get_queryset(self, *args, **kwargs):
-        qs = Tweet.objects.all().order_by("-timestamp")
-        print(self.request.GET)
+        im_following = self.request.user.profile.get_following() # none
+        qs = Tweet.objects.filter(user__in=im_following).order_by("-timestamp")
         query = self.request.GET.get("q", None)
         if query is not None:
             qs = qs.filter(
