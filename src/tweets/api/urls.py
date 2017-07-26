c6ef98a5051ed5e16f585e0562a385897b4fcfa5
Patricia Chun <yejeechun@gmail.com>
2017-07-26 19:45:51 -0400

    Single detail tweet.

--- src/tweets/api/urls.py
+++ src/tweets/api/urls.py
@@ -7,12 +7,15 @@ from .views import (
     RetweetAPIView,
     TweetCreateAPIView,
     TweetListAPIView,
+    TweetDetailAPIView,
+
     )
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
     url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /tweet/create
+    url(r'^(?P<pk>\d+)/$', TweetDetailAPIView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/like/$', LikeToggleAPIView.as_view(), name='like-toggle'), # /api/tweet/id/retweet
     url(r'^(?P<pk>\d+)/retweet/$', RetweetAPIView.as_view(), name='retweet'), # /api/tweet/id/retweet
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1/
