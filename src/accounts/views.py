1f2fe141b134a99a29bc39b896a29e1764a607b0
Patricia Chun <yejeechun@gmail.com>
2017-07-27 17:24:40 -0400

    Recommended users & custom template tags.

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -24,6 +24,8 @@ class UserDetailView(DetailView):
         context = super(UserDetailView, self).get_context_data(*args, **kwargs)
         following = UserProfile.objects.is_following(self.request.user, self.get_object())
         context['following'] = following
+        context['recommended'] = UserProfile.objects.recommended(self.request.user)
+
         return context
 
 class UserFollowView(View):
